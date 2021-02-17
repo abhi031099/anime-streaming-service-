139a2d0d6105b9d35486d21ca540a1fd2cd53019
abhi031099 <abhishek031099@gmail.com>
2021-02-18 02:03:24 +0530

    done with the main page design

--- Public/js/anime.js
+++ Public/js/anime.js
@@ -2,16 +2,9 @@ $(document).ready(function(){
     $("form.searchbar").submit(function(event){
         event.preventDefault();
         var searchQuery = $("#searchbox").val();   
-        console.log("http://sidanmor.com/"+searchQuery)
         window.location.href ="/animepage/"+searchQuery
     });
 });
-function checkDigits(no){
-    return no.toString().length;
-}
-
-
-
 
 function searchPage(){
    var pageNo = $('#pageno').val();
@@ -19,14 +12,5 @@ function searchPage(){
     var urlName = '/animelist/'
     var alphaName = pathName.replace(urlName,'')
     var myelement = alphaName.slice(0,1)
-  // console.log(myelement)
    window.location.href =urlName + myelement + '/'+ pageNo 
- // var k = checkDigits(pageno);
- //  if (k == 2){
- //   var res = str.slice(0, 5);
- //  }
-  // console.log(pageno,globalvar);
-  // window.location.replace('/animelist/0/'+ pageno)
-  // window.location.href ='/animelist/0/' +pageno
-
 }
--- app.js
+++ app.js
@@ -1,51 +1,20 @@
 var express = require('express');
-var mongoose = require('mongoose')
 var bodyParser = require('body-parser');
-const path=require("path");
 var app=express();
-var AnimeInfo = require('./model/anime');
-var videopageRoute = require('./routes/vidpage')
 var mainpageRoute = require('./routes/mainpage')
 var episodepageRoute = require('./routes/episodepage')
 var animepageRoute = require('./routes/animepage')
 var animelistRoute = require('./routes/animelist')
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const api = require('gogoanime')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set('view engine','ejs')
 app.use(express.static(__dirname + '/Public'));
-
-//connecting to the database
-mongoose.connect('mongodb+srv://abhishek_sharma:testwebsite@initiald.jigbi.mongodb.net/anime_info?retryWrites=true&w=majority',
-{useNewUrlParser: true,useUnifiedTopology:true})
-mongoose.connection.once('open', function(){
-    console.log("connected to the database")
-}).on('error', function(error){
-    comsole.log('error faced:', error)
-}) 
+// ROUTING
 app.use('/',mainpageRoute)
-app.use('/movie',videopageRoute)
 app.use('/episodepage',episodepageRoute)
 app.use('/animepage',animepageRoute)
 app.use('/animelist', animelistRoute)
-
-/*
-api.popular(1).then(function(result){
-    const lol = result
-    console.log(lol)
-})  
-api.search('my hero').then(function(result){
-    console.log(result[0].episodes[0].id)
-}) 
-api.search().then(function(result){
-    console.log(result)
-}) 
-
-
-app.listen("3000", function(req,res){
-    console.log("server running bois")
-}) 
-*/
+// INITIATING PORT REQ
 app.listen(process.env.PORT || 3000)
